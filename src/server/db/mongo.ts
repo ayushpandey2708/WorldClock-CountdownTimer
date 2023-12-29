6b4299353324e5d731427e08b6026477cd8d2885
Ayush <ayush.27.08.2000@gmail.com>
2023-12-29 12:06:31 +0530

    fix:resolved type error in mongo.ts file

--- src/server/db/mongo.ts
+++ src/server/db/mongo.ts
@@ -3,14 +3,26 @@ import { MongoClient, Db, Collection } from 'mongodb';
 let client: MongoClient;
 let logsCollection: Collection;
 
-const connectToMongoDB = async () => {
-    if (!client) {
-        client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect();
-    }
+const mongodbUri = process.env.MONGODB_URI 
 
-    const db: Db = client.db('timer');
-    logsCollection = db.collection('logs');
+const connectToMongoDB = async () => {
+    try{
+        if (!client) {
+
+            if (!mongodbUri) {
+                throw new Error('MONGODB_URI environment variable is not defined.');
+            }
+
+            client = new MongoClient(mongodbUri);
+            await client.connect();
+        }
+
+        const db: Db = client.db('timer');
+        logsCollection = db.collection('logs');
+    } 
+    catch (error:any) {
+        console.error(`An error occurred: ${error.message}`);
+    }
 };
 
 export { connectToMongoDB, logsCollection };
